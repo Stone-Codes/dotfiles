9f05c9ee44b615e6c4161018677f73ccea89f5bd
Florian Dietz <adminfd@Mac.fritz.box>
2026-07-16 15:48:11 +0200

    feat(nvim): add Avante keybindings

---
+++ neovim/.config/nvim/after/plugin/avante.lua
@@ -0,0 +1,10 @@
+local function open_avante_guide()
+  vim.cmd.edit(vim.fn.stdpath("config") .. "/AVANTE.md")
+end
+
+vim.keymap.set("n", "<leader>aa", "<cmd>AvanteToggle<CR>", { desc = "Avante: toggle sidebar" })
+vim.keymap.set({ "n", "v" }, "<leader>ac", "<cmd>AvanteAsk<CR>", { desc = "Avante: ask Codex" })
+vim.keymap.set("n", "<leader>af", "<cmd>AvanteFocus<CR>", { desc = "Avante: focus sidebar" })
+vim.keymap.set("n", "<leader>ar", "<cmd>AvanteRefresh<CR>", { desc = "Avante: refresh" })
+vim.keymap.set("n", "<leader>as", "<cmd>AvanteStop<CR>", { desc = "Avante: stop request" })
+vim.keymap.set("n", "<leader>ah", open_avante_guide, { desc = "Avante: open guide" })
